8cd92b7fabb90ad8abcee00d0cb1c17b3b8036b9
Ahmed <ahmedfayyaz1995@gmail.com>
2024-07-26 02:12:36 +0500

    Initial commit

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-neutral-950" >
+    <SignIn />
+    </div>;
+}
---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,7 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-neutral-950" >
+    <SignUp />
+    </div>;
+}
